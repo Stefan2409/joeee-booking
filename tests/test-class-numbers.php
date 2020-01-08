1257a299d68d5b5c59f77e981612b5340e1300bc
Stefan Jöbstl <stefan.joebstl@gmx.at>
2020-01-08 22:48:37 +0100

    Created tests for Numbers::round_up()

--- tests/test-class-numbers.php
+++ tests/test-class-numbers.php
@@ -19,6 +19,10 @@ class Numbers_VerifyTest extends WP_UnitTestCase {
     var $output_number_pl0 = 35;
     var $output_number_pl1 = 34.60;
     var $output_number_pl2 = 34.55;
+    var $negative_number = -23.513;
+    var $output_negative_number = -23;
+    var $output_negative_number_pl1 = -23.5;
+    var $output_negative_number_pl2 = -23.51;
 
     public function setUp() {
         parent::setUp();
@@ -29,6 +33,41 @@ class Numbers_VerifyTest extends WP_UnitTestCase {
         $rounded = $this->class_instance->round_up($this->input_number);
 
         $this->assertEquals($this->output_number_pl0, $rounded);
+		
+    }
+    
+    public function test_round_up_pl1() {
+        $rounded = $this->class_instance->round_up($this->input_number, 1);
+
+        $this->assertEquals($this->output_number_pl1, $rounded);
+		
+    }
+    
+    public function test_round_up_pl2() {
+        $rounded = $this->class_instance->round_up($this->input_number, 2);
+
+        $this->assertEquals($this->output_number_pl2, $rounded);
+		
+    }
+    
+    public function test_round_up_negative() {
+        $rounded = $this->class_instance->round_up($this->negative_number);
+
+        $this->assertEquals($this->output_negative_number, $rounded);
+		
+    }
+    
+    public function test_round_up_negative_pl1() {
+        $rounded = $this->class_instance->round_up($this->negative_number, 1);
+
+        $this->assertEquals($this->output_negative_number_pl1, $rounded);
+		
+    }
+    
+    public function test_round_up_negative_pl2() {
+        $rounded = $this->class_instance->round_up($this->negative_number, 2);
+
+        $this->assertEquals($this->output_negative_number_pl2, $rounded);
 		
 	}
 }
