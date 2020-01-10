08a0636e83ed41fc22063bcd258ea6e5d1388384
Stefan Jöbstl <stefan.joebstl@gmx.at>
2020-01-10 11:51:40 +0100

    Created test for the Strings string_ends_with() method.

---
+++ tests/test-class-strings.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Class Strings_Test
+ *
+ * @package Joeee_Booking
+ */
+
+/**
+ * STrings test case.
+ */
+use Joeee_Booking\Common\Utilities\Strings as Strings;
+
+class Strings_Test extends WP_UnitTestCase {
+    /**
+	 * Tests the methods of the Strings class 
+	 */
+    
+    
+    public function setUp() {
+        parent::setUp();
+
+        $this->class_instance = new Strings();
+    }
+
+    public function test_string_ends_with() {
+        $input_string = "Hello, World! Are we still waiting?";
+        $search_string = "we still waiting?";
+
+        $function_return = $this->class_instance->test_string_ends_with($input_string, $search_string);
+
+        $this->assertTrue($function_return);
+    }
+
+    public function test_string_ends_with_too_long__search_string() {
+        $input_string = "Hello, World! Are we still waiting?";
+        $search_string = "Hello, World! Are we still waiting? No?";
+
+        $function_return = $this->class_instance->test_string_ends_with($input_string, $search_string);
+
+        $this->assertFalse($function_return);
+    }
+
+    public function test_string_ends_with_not_a_string() {
+        $input_string = "Hello, World! Are we still waiting?";
+        $search_string = 13;
+
+        $function_return = $this->class_instance->test_string_ends_with($input_string, $search_string);
+
+        $this->assertFalse($function_return);
+    }
+}
