239c9600694fe2faafd442f9dce5294a263856cd
Stefan Jöbstl <stefan.joebstl@gmx.at>
2020-01-13 08:06:55 +0100

    Altered test for Links class.

---
+++ tests/test-class-links.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Class Links_Test
+ *
+ * @package Joeee_Booking
+ */
+
+/**
+ * Links test case.
+ */
+use Joeee_Booking\Common\Utilities\Links as Links;
+
+
+
+class Links_Test extends WP_UnitTestCase {
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->class_instance = new Links();
+    }
+
+    public function test_get_current_url() {
+        $link = $this->class_instance->get_current_url();
+
+
+        $this->assertEquals("http://example.org", $link);
+    }
+}
